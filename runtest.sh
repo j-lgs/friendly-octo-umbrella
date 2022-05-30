5e67ffe2d6c6ea8e63a1a977cf8db275e3b3401c
Jacob Logan <103975454+j-lgs@users.noreply.github.com>
2022-05-30 14:29:41 +1000

    a

--- runtest.sh
+++ runtest.sh
@@ -15,5 +15,14 @@ trap cleanup EXIT
 
 unshare --net --mount sh -c 'sleep 360' &
 pid="$!"
+<<<<<<< HEAD
 ./slirp4netns --configure --mtu=65520 --disable-host-loopback $pid tap9 > /dev/null 2>&1 &
 nsenter --wd="$(pwd)" -t $pid -m -n --preserve ./runvm.sh
+=======
+./slirp4netns --configure --mtu=65520 "$pid" tap0 > /dev/null 2>&1 &
+slirppid="$!"
+nsenter --wd="$(pwd)" -t "$pid" -U -m -n --preserve runvm.sh
+
+kill "$pid"
+kill "$slirppid"
+>>>>>>> e68ef94 (a)
