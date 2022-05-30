3385d06758a8f882ec1a17e5e63f082ec99a5d0e
Jacob Logan <103975454+j-lgs@users.noreply.github.com>
2022-05-30 21:00:08 +1000

    kvmtest

--- runvm.sh
+++ runvm.sh
@@ -19,10 +19,6 @@ trap cleanup EXIT
 
 timeout="120"
 
-tapup() {
-    
-}
-
 ip link add br0 type bridge
 ip link set dev tap9 master br0
 ip link set dev br0 up
@@ -54,13 +50,13 @@ RAND_MAC_2="de:ad:be:ef:ec:72"
 RAND_MAC_3="de:ad:be:ef:88:c0"
 RAND_MAC_4="de:ad:be:ef:41:1c"
 
-qemu-system-x86_64 -m 1024 -cdrom talos-amd64.iso -netdev tap,id=mynet0,ifname=tap0,script=no,downscript=no -device virtio-net-pci,netdev=mynet0,mac=$RAND_MAC_1 -serial file:serial-1.log -display none -daemonize -pidfile ./qemu-1.pid
+qemu-system-x86_64 -m 1024 -cdrom talos-amd64.iso -netdev tap,id=mynet0,ifname=tap0,script=no,downscript=no -device virtio-net-pci,netdev=mynet0,mac=$RAND_MAC_1 -serial file:serial-1.log -display none -daemonize -pidfile ./qemu-1.pid --enable-kvm
 
-qemu-system-x86_64 -m 1024 -cdrom talos-amd64.iso -netdev tap,id=mynet0,ifname=tap1,script=no,downscript=no -device virtio-net-pci,netdev=mynet0,mac=$RAND_MAC_2 -serial file:serial-2.log -display none -daemonize -pidfile ./qemu-2.pid
+qemu-system-x86_64 -m 1024 -cdrom talos-amd64.iso -netdev tap,id=mynet0,ifname=tap1,script=no,downscript=no -device virtio-net-pci,netdev=mynet0,mac=$RAND_MAC_2 -serial file:serial-2.log -display none -daemonize -pidfile ./qemu-2.pid --enable-kvm
 
-qemu-system-x86_64 -m 1024 -cdrom talos-amd64.iso -netdev tap,id=mynet0,ifname=tap2,script=no,downscript=no -device virtio-net-pci,netdev=mynet0,mac=$RAND_MAC_3 -serial file:serial-3.log -display none -daemonize -pidfile ./qemu-3.pid
+qemu-system-x86_64 -m 1024 -cdrom talos-amd64.iso -netdev tap,id=mynet0,ifname=tap2,script=no,downscript=no -device virtio-net-pci,netdev=mynet0,mac=$RAND_MAC_3 -serial file:serial-3.log -display none -daemonize -pidfile ./qemu-3.pid --enable-kvm
 
-qemu-system-x86_64 -m 1024 -cdrom talos-amd64.iso -netdev tap,id=mynet0,ifname=tap3,script=no,downscript=no -device virtio-net-pci,netdev=mynet0,mac=$RAND_MAC_4 -serial file:serial-4.log -display none -daemonize -pidfile ./qemu-4.pid
+qemu-system-x86_64 -m 1024 -cdrom talos-amd64.iso -netdev tap,id=mynet0,ifname=tap3,script=no,downscript=no -device virtio-net-pci,netdev=mynet0,mac=$RAND_MAC_4 -serial file:serial-4.log -display none -daemonize -pidfile ./qemu-4.pid --enable-kvm
 
 tail -f serial-1.log &
 
