19b118ddb2b50ec927e0521383b61b1006f497a6
Jacob Logan <103975454+j-lgs@users.noreply.github.com>
2022-05-30 16:25:21 +1000

    a

--- runvm.sh
+++ runvm.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # kill parent processes
 cleanup() {
+    kill $(cat ./qemu.pid)
     pkill -P $$
 }
 
@@ -13,12 +14,25 @@ for sig in INT QUIT HUP TERM; do
 done
 trap cleanup EXIT
 
-qemu-system-x86_64 -m 512 talos-amd64.iso -netdev user,id=mynet0 -device e1000,netdev=mynet0 -serial file:serial.log -display none -daemonize
-echo $! > /tmp/vmpid
+timeout="30"
 
+ip link add br0 type bridge
+ip tuntap add dev tap0 mode tap
+ip link set dev tap0 master br0
+ip link set dev tap9 master br0
+ip link set dev br0 up
+
+ip address delete 10.0.2.100/24 dev tap9
+ip address add 10.0.2.100/24 dev br0
+ip route add default via 10.0.2.100 dev br0
+
+ip link set tap0 up
+ip a
+
+qemu-system-x86_64 -m 512 -cdrom talos-amd64.iso -netdev tap,id=mynet0,ifname=tap0,script=no,downscript=no -device virtio-net-pci,netdev=mynet0 -serial file:serial.log -display none -daemonize -pidfile ./qemu.pid
 tail -f serial.log &
-echo $! > /tmp/tailpid
+tailpid="$!"
 
-sleep 60
-
-kill $(cat /tmp/vmpid) $(cat /tmp/tailpid)
+sleep "$timeout"
+ping -c 5 10.0.2.15
+ip neigh
