b8207750c8fa52c16708b052ca6544ba6694e1dd
Jacob Logan <103975454+j-lgs@users.noreply.github.com>
2022-05-30 14:10:44 +1000

    Update runtest.sh
    fix no gtk

--- runtest.sh
+++ runtest.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # kill parent processes
 cleanup() {
-    pkill -P $$
+   pkill -P $$
 }
 
 # Setup signals to kill child processes on exit.
@@ -13,12 +13,7 @@ for sig in INT QUIT HUP TERM; do
 done
 trap cleanup EXIT
 
-unshare --user --map-root-user --net --mount sh -c 'sleep 1800' &
+unshare --net --mount sh -c 'sleep 360' &
 pid="$!"
-./slirp4netns --configure --mtu=65520 "$pid" tap0 > /dev/null 2>&1 &
-slirppid="$!"
-nsenter --wd="$(pwd)" -t "$pid" -U -m -n --preserve runvm.sh
-sh -c 'qemu-system-x86_64 -m 512 talos-amd64.iso -netdev user,id=mynet0 -device e1000,netdev=mynet0 --serial mon:stdio'
-
-kill "$pid"
-kill "$slirppid"
+./slirp4netns --configure --mtu=65520 --disable-host-loopback $pid tap9 > /dev/null 2>&1 &
+nsenter --wd="$(pwd)" -t $pid -m -n --preserve ./runvm.sh
