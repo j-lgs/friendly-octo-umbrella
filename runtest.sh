a71fc7869d39e106b5147aca67e82a804da9e388
Jacob Logan <103975454+j-lgs@users.noreply.github.com>
2022-05-30 21:04:26 +1000

    hs

--- runtest.sh
+++ runtest.sh
@@ -13,13 +13,5 @@ for sig in INT QUIT HUP TERM; do
 done
 trap cleanup EXIT
 
-touch serial-1.log && chown 1000:1000 serial-1.log
-touch serial-2.log && chown 1000:1000 serial-2.log
-touch serial-3.log && chown 1000:1000 serial-3.log
-touch serial-4.log && chown 1000:1000 serial-4.log
+hostname
 
-unshare --user --map-root-user --net --mount sh -c 'sleep 360' &
-pid=$!
-sleep 0.1
-./slirp4netns --configure --mtu=65520 --disable-host-loopback $pid tap9 > /dev/null &
-nsenter -U --wd="$(pwd)" -t $pid -m -n --preserve ./runvm.sh
